178ded7c1d8527295d7aa8f3995cf881c8d6f276
Sonny Tosco <sonny.tosco@gmail.com>
2016-10-06 10:29:22 -0700

    creating objects 2 completed

--- createobjects.js
+++ createobjects.js
@@ -1,32 +1,70 @@
-function VehicleConstructor(name, wheels, passengers){
-  var vehicle = {};
+// function VehicleConstructor(name, wheels, passengers){
+//   var vehicle = {};
+//
+//   vehicle.name = name;
+//   vehicle.wheels = wheels;
+//   vehicle.passengers = passengers;
+//
+//   vehicle.makeNoise = function(){
+//     console.log("This is me honking my bike horn");
+//   }
+//   return vehicle;
+// }
+//
+// var bike = VehicleConstructor("Scraper", "2", "2")
+// bike.makeNoise = function(){
+//   console.log("ring ring!")
+// }
+// bike.makeNoise();
+//
+// var Sedan = VehicleConstructor("Hooptie", 2, 3)
+// Sedan.makeNoise = function(){
+//   console.log("Honk Honk!")
+// }
+// Sedan.makeNoise();
+//
+// var bus = VehicleConstructor("Public limo", 2, 40)
+// bus.pickUp = function(passengers){
+//   bus.passengers += passengers;
+// }
+// console.log(bus.passengers);
+// bus.pickUp(14);
+// console.log(bus.passengers);
 
-  vehicle.name = name;
-  vehicle.wheels = wheels;
-  vehicle.passengers = passengers;
+//Creating Objects II
+new VehicleConstructor();
 
-  vehicle.makeNoise = function(){
-    console.log("This is me honking my bike horn");
+function VehicleConstructor(name, wheels, passengers, speed){
+  if(!(this instanceof VehicleConstructor)){
+    return new VehicleConstructor(name, wheels, passengers, speed);
   }
-  return vehicle;
+  var self = this;
+//private vars
+  var distance_traveled = 0;
+//private methods
+  function updateDistanceTraveled(){
+    distance_traveled += self.speed;
+  }
+
+//public vars
+  this.name = name || "hooptie";
+  this.wheels = wheels || 4;
+  this.passengers = passengers || 3;
+  this.speed = speed || 15;
+
+  this.makeNoise = function(noise){
+    var noise = noise || "Honk honk";
+    console.log(noise);
+  }
+  this.move = function(){
+    updateDistanceTraveled();
+    this.makeNoise();
+  }
+  this.checkMiles = function(){
+    return distance_traveled;
+  }
 }
 
-var bike = VehicleConstructor("Scraper", "2", "2")
-bike.makeNoise = function(){
-  console.log("ring ring!")
-}
-bike.makeNoise();
-
-var Sedan = VehicleConstructor("Hooptie", 2, 3)
-Sedan.makeNoise = function(){
-  console.log("Honk Honk!")
-}
-Sedan.makeNoise();
-
-var bus = VehicleConstructor("Public limo", 2, 40)
-bus.pickUp = function(passengers){
-  bus.passengers += passengers;
-}
-console.log(bus.passengers);
-bus.pickUp(14);
-console.log(bus.passengers);
+var bus = VehicleConstructor('Bus', 8, 35, 60);
+console.log(bus.speed);
+// console.log(global);
