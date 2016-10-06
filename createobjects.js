abc25e7ef0a780ba95342c0cff87e0c8e8aa32ea
Sonny Tosco <sonny.tosco@gmail.com>
2016-10-06 12:06:47 -0700

    starting creating objects3

--- createobjects.js
+++ createobjects.js
@@ -31,7 +31,44 @@
 // bus.pickUp(14);
 // console.log(bus.passengers);
 
-//Creating Objects II
+// //Creating Objects II
+// new VehicleConstructor();
+//
+// function VehicleConstructor(name, wheels, passengers, speed){
+//   if(!(this instanceof VehicleConstructor)){
+//     return new VehicleConstructor(name, wheels, passengers, speed);
+//   }
+//   var self = this;
+// //private vars
+//   var distance_traveled = 0;
+// //private methods
+//   function updateDistanceTraveled(){
+//     distance_traveled += self.speed;
+//   }
+//
+// //public vars
+//   this.name = name || "hooptie";
+//   this.wheels = wheels || 4;
+//   this.passengers = passengers || 3;
+//   this.speed = speed || 15;
+//
+//   this.makeNoise = function(noise){
+//     var noise = noise || "Honk honk";
+//     console.log(noise);
+//   }
+//   this.move = function(){
+//     updateDistanceTraveled();
+//     this.makeNoise();
+//   }
+//   this.checkMiles = function(){
+//     return distance_traveled;
+//   }
+// }
+//
+// var bus = VehicleConstructor('Bus', 8, 35, 60);
+// console.log(bus.speed);
+// console.log(global);
+
 new VehicleConstructor();
 
 function VehicleConstructor(name, wheels, passengers, speed){
@@ -67,4 +104,3 @@ function VehicleConstructor(name, wheels, passengers, speed){
 
 var bus = VehicleConstructor('Bus', 8, 35, 60);
 console.log(bus.speed);
-// console.log(global);
